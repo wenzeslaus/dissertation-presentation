1d418b26c76270062374518092cd707c7bb12fd0
Vaclav Petras <wenzeslaus@gmail.com>
2017-03-10 16:16:24 -0500

    Initial commit of the website

---
+++ build.sh
@@ -0,0 +1,8 @@
+./copy-common-files.py --dst-dir=build
+
+./build-slides.py --outdir=build \
+    --outfile=index.html \
+    --title="" \
+    --meta-description="" \
+    --meta-author="Vaclav Petras" \
+    index.html
