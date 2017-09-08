639102e43560b177afd39be0512d8766e0aa7018
Vaclav Petras <wenzeslaus@gmail.com>
2017-09-08 11:40:59 -0400

    abandon build scripts (use Meld to sync if needed)

--- build.sh
+++
@@ -1,8 +0,0 @@
-./copy-common-files.py --dst-dir=build
-
-./build-slides.py --outdir=build \
-    --outfile=index.html \
-    --title="" \
-    --meta-description="" \
-    --meta-author="Vaclav Petras" \
-    index.html
