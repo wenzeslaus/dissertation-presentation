8522a7a74fc5dcd448d3413bdbb31c749aa8d587
Vaclav Petras <wenzeslaus@gmail.com>
2018-04-21 16:59:44 -0400

    larger reproducibility ratio chart, replace table

--- chart_ratio.py
+++ chart_ratio.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 
 def format_label(value):
     return '{:0.0f}%'.format(value)
 
+
+mpl.rcParams['font.size'] = 17.0
 
 fig, ax = plt.subplots()
 ax.axis('equal')
@@ -14,19 +17,25 @@ values = [26, 74]
 text_labels = ['reproducible', 'irreproducible']
 colors = ['#6D9CCE', '#F13F52']
 
+text_labels = [
+    '{value}% {text}'.format(text=text, value=value)
+    for text, value in zip(text_labels, values)
+    ]
+
+radius = 1.36
 width = 0.5
-label_position = 1 - width / 2
+label_position = 0.8
 start_angle = 10
 
 circle, _, _ = ax.pie(
     values, colors=colors,
-    radius=1, startangle=start_angle,
+    radius=radius, startangle=start_angle,
     autopct=format_label, pctdistance=label_position
     )
 
 plt.setp(circle, width=width, edgecolor='white')
 
-ax.legend(circle, text_labels, frameon=False)
+ax.legend(circle, text_labels, frameon=False, loc='center')
 
 plt.tight_layout()
 
